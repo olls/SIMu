5f4fa39765ae7a6799837a3151d8565b8d04de65
Oliver Faircliff <mail@oliverfaircliff.com>
2014-11-29 20:45:49 -0500

    Fix invaders gap

--- index.js
+++ index.js
@@ -97,8 +97,8 @@ function gen_invaders () {
       var e_id = id++;
       entities[e_id] = {
         type: 'invader_1',
-        x: (x*inv_w) + 30,
-        y: (y*inv_h) + 30
+        x: (x+10) * inv_w,
+        y: (y+10) * inv_h
       };
     }
   }
